7d829651fa1fd700f441fd9cb0de71779837e2b1
Brian Barrett <blaxbb@gmail.com>
2020-11-18 20:58:51 -0500

    Secure file delivery

---
+++ LocatorServer/Controllers/SecureFileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocatorServer.Controllers
+{
+    [Authorize("authorized")]
+    public class SecureFileController : Controller
+    {
+        [HttpGet]
+        [Route("securefile/{filename}")]
+        public IActionResult Get(string filename)
+        {
+            return PhysicalFile($"/app/Data/SecureFiles/{filename}", "applictaion/text");
+        }
+    }
+}
