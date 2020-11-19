cd5bfb9f00a8909f0e54dbf4ca0a5cff589c759a
Brian Barrett <blaxbb@gmail.com>
2020-11-18 22:52:17 -0500

    secure files use correct content-type

--- LocatorServer/Controllers/SecureFileController.cs
+++ LocatorServer/Controllers/SecureFileController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using LocatorServer.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +9,35 @@ using System.Threading.Tasks;
 
 namespace LocatorServer.Controllers
 {
-    [Authorize("authorized")]
-    public class SecureFileController : Controller
+    [ApiController]
+    public class SecureFileController : ControllerBase
     {
+        /*
+         * 
+         * 
+         * There is a pretty annoying "bug" in aspnet core 5 where using both mvc and webapi will
+         * cause the webapi functions to redirect to the login page instead of just returning
+         * 401.
+         * 
+         * Ideally, I would just throw an [Authorize(Roles="authorized")] on the class, but
+         * instead I have to manually return Unauthorized for each method.  This is fine, but
+         * should not be ignored in the future!!!!!
+         * 
+         * https://github.com/dotnet/aspnetcore/issues/9039
+         * 
+         * 
+         */
         [HttpGet]
-        [Route("securefile/{filename}")]
+        [Route("api/securefile/{filename}")]
         public IActionResult Get(string filename)
         {
-            return PhysicalFile($"/app/Data/SecureFiles/{filename}", "applictaion/text");
+            if (!User.IsInRole("authorized"))
+            {
+                return Unauthorized();
+            }
+            var path = $"/app/Data/SecureFiles/{filename}";
+
+            return PhysicalFile(path, "image/png");
         }
     }
 }
